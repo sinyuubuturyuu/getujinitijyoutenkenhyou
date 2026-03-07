bbfbdbf9c719a99ebb2083e91e8c1dca387d3636
yuu <yuuiti.htnk167@gmail.com>
2026-03-07 17:25:40 +0900

    Initial app setup and holiday handling updates

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+﻿window.APP_FIREBASE_CONFIG = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+window.APP_SETTINGS = {
+  collectionName: "monthlyInspectionEntries",
+  useLocalFallbackWhenFirebaseIsMissing: true
+};
